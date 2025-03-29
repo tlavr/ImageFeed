0da144c57940c94c50ecd28f40605d1dbcee1132
Timur Lavrukhin <t.lavrukhin@gmail.com>
2025-03-29 18:32:17 +0200

    Initial Commit

---
+++ ImageFeedUITests/ImageFeedUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ImageFeedUITestsLaunchTests.swift
+//  ImageFeedUITests
+//
+//  Created by Timur Lavrukhin on 29.3.2025.
+//
+
+import XCTest
+
+final class ImageFeedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
