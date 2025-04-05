8e8f6ef1fd1b1e6e3bf559126be573010e377b62
Timur Lavrukhin <t.lavrukhin@gmail.com>
2025-04-05 18:23:27 +0300

    feature(mock images feed) : Add mock images feed according to sprint 08 requirements

---
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Timur Lavrukhin on 29.3.2025.
+//
+
+import UIKit
+
+final class ImagesListViewController: UIViewController {
+    // MARK: - IBOutlets
+    @IBOutlet private var tableView: UITableView!
+    
+    // MARK: -Private properties
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    // MARK: - View state handlers
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = UIColor(named: "YP Black (iOS)")
+        tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
+    }
+    
+    // MARK: - Private methods
+    private func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let newImage = UIImage(named: photosName[indexPath.row]) else { return }
+        cell.cellImage.image = newImage
+        cell.dateLabel.text = dateFormatter.string(from: Date())
+        cell.likeButton.setTitle("", for: .normal)
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.setImage(UIImage(named: "LikeActive"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "LikeNotActive"), for: .normal)
+        }
+    }
+}
+
+extension ImagesListViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photosName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImagesListCell else { return UITableViewCell() }
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
+    }
+}
+
+extension ImagesListViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else { return 0 }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
+}
+
