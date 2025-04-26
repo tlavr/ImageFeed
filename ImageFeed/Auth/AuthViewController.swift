0fe7cea74660caf3437ede37aa6bdec74f3eff53
Timur Lavrukhin <t.lavrukhin@gmail.com>
2025-04-27 01:48:31 +0300

    Feature(Auth): Add AuthView and WebView controllers. Add segue

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Timur Lavrukhin on 27.4.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    // MARK: -Private properties
+    private let showWebViewSegueIdentifier = "showWebView"
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Timur Lavrukhin on 27.4.2025.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+    
+    
+}
